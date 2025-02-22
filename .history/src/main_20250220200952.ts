1c423ab6d63e0968473be1c6f8f1d7b415c1a471
Dulce Diaz <2516160077ddiazs@gmail.com>
2025-02-21 18:35:53 -0600

    dulce graphics 1 v

---
+++ .history/src/main_20250220200952.ts
@@ -0,0 +1,31 @@
+//import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createVuetify } from 'vuetify'
+import * as labs from 'vuetify/labs/components'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import './styles/main.scss'
+
+import App from './App.vue'
+import router from './router'
+
+import axios from 'axios'
+
+const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  labs,
+  directives,
+})
+
+app.use(createPinia())
+app.use(router)
+app.use(vuetify)
+app.use(axios)
+
+app.mount('#app')
